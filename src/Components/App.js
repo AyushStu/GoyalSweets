f7df58caf99dd348f3b1c7746a49e47338e4905b
AyushStu <ayushgoyal1235@gmail.com>
2025-09-07 23:30:10 +0530

    New

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import "../Css/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Sweets from "./Sweets";
 import Namkeen from "./Namkeen";
